dbc22308cfbcb38991d0ee6669e7db5344ee90c3
MINAS\User <ics20072@uom.edu.gr>
2023-01-03 23:10:34 +0200

    no message

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -15,10 +15,10 @@ export default {
   },
   async auth(context, payload) {
     const mode = payload.mode;
-    let url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBbEyLjA5LQ7JGkWbc70CQ6hvIakwQZnnA`;
+    let url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.VUE_APP_FIREBASE_WEB_API_KEY}`;
 
     if (mode === 'signup') {
-      url = ` https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBbEyLjA5LQ7JGkWbc70CQ6hvIakwQZnnA`;
+      url = ` https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.VUE_APP_FIREBASE_WEB_API_KEY}`;
     }
 
     const response = await fetch(url, {
